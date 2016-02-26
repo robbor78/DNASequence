b04c0d04f52032ea3e776320514e923c264a24b5
username <email@email.com>
2016-02-26 14:58:49 +0200

    DNASequence topcoder

---
+++ SRM682DNASequence/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRM682DNASequence
+{
+  public class Program
+  {
+    static void Main(string[] args)
+    {
+    }
+
+    public int longestDNASequence(String sequence)
+    {
+      int max = 0;
+      int count = 0;
+
+      string allowed = "ACGT";
+
+      for (int i=0; i<sequence.Length; i++)
+      {
+        char c = sequence[i];
+        if (allowed.Contains(c))
+        {
+          count++;
+        } else
+        {
+          max = count > max ? count : max;
+          count = 0;
+        }
+      }
+
+      if (count!=0)
+      {
+        max = count > max ? count : max;
+      }
+
+      return max;
+    }
+  }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SRM682DNASequence;
+
+namespace UnitTestProject1
+{
+  [TestClass]
+  public class UnitTest1
+  {
+    [TestMethod]
+    public void TestMethod1()
+    {
+      string[] allSequences = new string[]
+      {
+"TOPBOATER",
+"SUSHI",
+"GATTACA",
+"GOODLUCK",
+"VVZWKCSIQEGANULDLZESHUYHUQGRKUMFCGTATGOHMLKBIRCA"
+      };
+
+      int[] allMax = new int[] { 2, 0, 7, 1, 6 };
+
+      Program p = new Program();
+      int length = allSequences.Length;
+      for (int i = 0; i < length; i++)
+      {
+        string sequence = allSequences[i];
+        int actual = p.longestDNASequence(sequence);
+        int expected = allMax[i];
+        Assert.AreEqual(expected, actual);
+      }
+    }
+  }
+}
